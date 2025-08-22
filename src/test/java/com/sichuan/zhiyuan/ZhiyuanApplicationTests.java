a07bb0a800a454d2c289a0adf2a5f625f35fe816
刘俊 <1061999312@qq.com>
2025-08-22 21:54:52 +0800

    init:代码初始化

---
+++ src/test/java/com/sichuan/zhiyuan/ZhiyuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sichuan.zhiyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhiyuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
